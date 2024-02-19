b42016a9057a3e9ac7f21222e4491ae75ed03151
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-02-19 08:36:32 +0100

    feat(gateway): improved sheleton and test

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const app = express();
+const cors= require('cors');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const router = require('./src/routes/route');
+
+/*
+* Allow access from any subroute of http://localhost:3000
+* */
+let corsOptions = {
+  origin: /http:\/\/localhost:3000\/.*/
+};
+
+
+const app = express();
+app.use(cors(corsOptions));
+
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
