b60780a0f4ade541cf781a91894c955a1c8fdaa2
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-02-19 08:12:48 +0100

    feat(gateway): gateway scheleton files

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+const {app} = require("../index");
+const request= require("supertest");//simula le richieste HTTP al tuo server Express e verifica le risposte
+
+describe('Index Testing', () => {
+    it('Should respond with status 200 for the main route', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+    });
+
+    it('should respond with "Hello World!" at the root endpoint', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Hello World!');
+      });
+});
